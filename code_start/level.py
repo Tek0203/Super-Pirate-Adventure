b2c0deaac5ea56a15ebf4bf0485e2bddeb596334
techwiz2003 <vsherron237@gmail.com>
2026-06-17 17:03:05 -0500

    Working on UI & Interactivity. June 17th 2026

--- code_start/level.py
+++ code_start/level.py
@@ -1,12 +1,13 @@
 from settings import *
-from sprites import Sprite, AnimatedSprite, MovingSprite, Spike
+from sprites import Sprite, AnimatedSprite, MovingSprite, Spike, Item, ParticleEffectSprite
 from player import Player
 from groups import AllSprites
-from enemies import Tooth, Shell
+from enemies import Tooth, Shell, Pearl
 
 class Level:
-    def __init__(self, tmx_map, level_frames):
+    def __init__(self, tmx_map, level_frames, data):
         self.display_surface = pygame.display.get_surface()
+        self.data = data
 
         # groups
         self.all_sprites = AllSprites()
@@ -14,8 +15,14 @@ class Level:
         self.semi_collision_sprites = pygame.sprite.Group()
         self.damaged_sprites = pygame.sprite.Group()
         self.tooth_sprites = pygame.sprite.Group()
+        self.pearl_sprites = pygame.sprite.Group()
+        self.item_sprites = pygame.sprite.Group()
 
         self.setup(tmx_map, level_frames)
+
+        # frames
+        self.pearl_surf = level_frames['pearl']
+        self.particle_frames = level_frames['particle']
 
     def setup(self, tmx_map, level_frames):
         # tiles
@@ -38,7 +45,8 @@ class Level:
                     groups = self.all_sprites, 
                     collision_sprites = self.collision_sprites, 
                     semi_collision_sprites = self.semi_collision_sprites,
-                    frames = level_frames['player'])
+                    frames = level_frames['player'],
+                    data = self.data)
             else:
                 if obj.name in ('barrel', 'crate'):
                     Sprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
@@ -100,9 +108,56 @@ class Level:
             if obj.name == 'tooth':
                 Tooth((obj.x, obj.y), level_frames['tooth'], (self.all_sprites, self.damaged_sprites, self.tooth_sprites), self.collision_sprites) 
             if obj.name == 'shell':
-                Shell((obj.x, obj.y), level_frames['shell'], (self.all_sprites, self.collision_sprites), obj.properties['reverse'], self.player)
+                Shell(
+                    pos = (obj.x, obj.y), 
+                    frames = level_frames['shell'], 
+                    groups = (self.all_sprites, self.collision_sprites), 
+                    reverse = obj.properties['reverse'], 
+                    player = self.player, 
+                    create_pearl = self.create_pearl)
+
+        # items
+        for obj in tmx_map.get_layer_by_name('Items'):
+            Item(obj.name, (obj.x + TILE_SIZE / 2, obj.y + TILE_SIZE / 2), level_frames['items'][obj.name], (self.all_sprites, self.item_sprites), self.data)
+
+    def create_pearl(self, pos, direction):
+        Pearl(pos, (self.all_sprites, self.damaged_sprites, self.pearl_sprites), self.pearl_surf, direction, 150)
+
+    def pearl_collision(self):
+        for sprite in self.collision_sprites:
+            sprite = pygame.sprite.spritecollide(sprite, self.pearl_sprites, True)
+            if sprite:
+                ParticleEffectSprite((sprite[0].rect.center), self.particle_frames, self.all_sprites)
+                
+    def hit_collision(self):
+        for sprite in self.damaged_sprites:
+            if sprite.rect.colliderect(self.player.hitbox_rect):
+                self.player.get_damaged()
+                if hasattr(sprite, 'pearl'):
+                    sprite.kill()
+                    ParticleEffectSprite((sprite.rect.center), self.particle_frames, self.all_sprites)
+
+    def item_collision(self):
+        if self.item_sprites:
+            item_sprites = pygame.sprite.spritecollide(self.player, self.item_sprites, True)
+            if item_sprites:
+                item_sprites[0].activate()
+                ParticleEffectSprite((item_sprites[0].rect.center), self.particle_frames, self.all_sprites)
+
+    def attack_collision(self):
+        for target in self.pearl_sprites.sprites() + self.tooth_sprites.sprites():
+            facing_target = self.player.rect.centerx < target.rect.centerx and self.player.facing_right or \
+                            self.player.rect.centerx > target.rect.centerx and not self.player.facing_right
+            if target.rect.colliderect(self.player.rect) and self.player.attacking and facing_target:
+                target.reverse()
 
     def run(self, dt):
         self.display_surface.fill('black')
+
         self.all_sprites.update(dt)
+        self.pearl_collision()
+        self.hit_collision()
+        self.item_collision()
+        self.attack_collision()
+
         self.all_sprites.draw(self.player.hitbox_rect.center)
--- code_start/main.py
+++ code_start/main.py
@@ -2,8 +2,10 @@ from settings import *
 from level import Level
 from pytmx.util_pygame import load_pygame
 from os.path import join
-
 from support import *
+from data import Data
+from debug import debug
+from ui import UI
 
 class Game:
 	def __init__(self):
@@ -13,8 +15,10 @@ class Game:
 		self.clock = pygame.time.Clock()
 		self.import_assets()
 
+		self.ui = UI(self.font, self.ui_frames)
+		self.data = Data(self.ui)
 		self.tmx_maps = {0: load_pygame(join('data/levels/omni.tmx'))}
-		self.current_stage = Level(self.tmx_maps[0], self.level_frames)
+		self.current_stage = Level(self.tmx_maps[0], self.level_frames, self.data)
 
 	def import_assets(self):
 		self.level_frames = {
@@ -29,7 +33,16 @@ class Game:
 			'spike': import_image('graphics/enemies/spike_ball/Spiked Ball'),
 			'spike_chain': import_image('graphics/enemies/spike_ball/spiked_chain'),
 			'tooth': import_folder('graphics/enemies/tooth/run'),
-			'shell': import_sub_folders('graphics/enemies/shell')
+			'shell': import_sub_folders('graphics/enemies/shell'),
+			'pearl': import_image('graphics/enemies/bullets/pearl'),
+			'items': import_sub_folders('graphics/items'),
+			'particle': import_folder('graphics/effects/particle')
+		}
+
+		self.font = pygame.font.Font(join('graphics/ui/runescape_uf.ttf'), 40)
+		self.ui_frames = {
+			'heart': import_folder('graphics/ui/heart'),
+			'coin': import_folder('graphics/ui/coin')
 		}
 
 	def run(self):
@@ -42,6 +55,7 @@ class Game:
 					sys.exit()
 
 			self.current_stage.run(dt)
+			self.ui.update(dt)
 			pygame.display.update()
 
 if __name__ == '__main__':
