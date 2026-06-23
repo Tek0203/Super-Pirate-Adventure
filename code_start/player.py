5ce11a6121caba14abd08f62350fd3d4fea54d57
techwiz2003 <vsherron237@gmail.com>
2026-06-23 13:24:57 -0500

    Levels connected to overworld & Added sounds and music. Tuesday June 23rd 2026

--- code_start/player.py
+++ code_start/player.py
@@ -4,7 +4,7 @@ from os.path import join
 from math import sin
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, collision_sprites, semi_collision_sprites, frames, data):
+    def __init__(self, pos, groups, collision_sprites, semi_collision_sprites, frames, data, attack_sound, jump_sound):
         # general setup
         super().__init__(groups)
         self.z = Z_LAYERS['main']
@@ -50,6 +50,10 @@ class Player(pygame.sprite.Sprite):
             'hit': Timer(400)
         }
 
+        # audio
+        self.attack_sound = attack_sound
+        self.jump_sound = jump_sound
+
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0,0)
@@ -79,6 +83,7 @@ class Player(pygame.sprite.Sprite):
             self.attacking = True
             self.frame_index = 0
             self.timers['attack block'].activate()
+            self.attack_sound.play()
 
     def move(self, dt):
         self.hitbox_rect.x += self.direction.x * self.speed * dt
@@ -249,10 +254,12 @@ class Player(pygame.sprite.Sprite):
                 self.direction.y = -self.jump_height
                 self.timers['wall slide block'].activate()
                 self.hitbox_rect.bottom -= 1
+                self.jump_sound.play()
             elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['wall slide block'].active:
                 self.timers['wall jump'].activate()
                 self.direction.y = -self.jump_height
                 self.direction.x = 1 if self.on_surface['left'] else -1
+                self.jump_sound.play()
             self.jump = False
 
         # vertical integration and wall-slide handling
